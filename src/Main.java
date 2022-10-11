f70eb6a2efd61477867cbf2317d2cab32f274c91
Alves <uo284288@uniovi.es>
2022-10-11 20:29:48 +0200

    Añadida una tercera línea al main()

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Bahamut!");
 		System.out.println("Rawr!");
+		/*Cambio desde rama 2*/
+		System.out.println("Roar!");
 	}
-
 }
